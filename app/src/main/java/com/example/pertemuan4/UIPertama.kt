c4374056a658ccfd73f75d17156554ca327c05db
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-10-23 16:20:35 +0700

    Mengatur agar semua elemen di dalam Column berada di tengah secara horizontal.

--- app/src/main/java/com/example/pertemuan4/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -4,9 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
 @Composable
 fun ActivitasPertama(modifier: Modifier){
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
     }
 }
