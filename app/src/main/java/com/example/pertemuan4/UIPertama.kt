31ad402358de69aa1b20f59293187a93547e102e
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-10-23 16:23:02 +0700

    Menambahkan Text pertama untuk menampilkan program studi.

--- app/src/main/java/com/example/pertemuan4/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -5,12 +5,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Alignment
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.stringResource
 @Composable
 fun ActivitasPertama(modifier: Modifier){
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Text(stringResource(R.string.prodi))
     }
 }
