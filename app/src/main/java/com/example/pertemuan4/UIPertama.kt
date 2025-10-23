204f7e4c4af349868f818b0e8df5dfddb256656d
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-10-23 16:17:58 +0700

    Menambahkan Column sebagai layout utama untuk menampung semua elemen secara vertikal.

--- app/src/main/java/com/example/pertemuan4/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -2,8 +2,11 @@ package com.example.pertemuan4
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 @Composable
 fun ActivitasPertama(modifier: Modifier){
+    Column(modifier = Modifier.fillMaxSize()) {
 
+    }
 }
