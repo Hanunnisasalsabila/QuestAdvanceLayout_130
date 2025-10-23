0f3a0681c656ce65507a26e799e716d7b66bea16
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-10-23 16:45:08 +0700

    Memastikan teks copyright selalu di posisi tengah bawah.

--- app/src/main/java/com/example/pertemuan4/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -1,47 +1,47 @@
 package com.example.pertemuan4
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Alignment
-import androidx.compose.material3.Text
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Card
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CardDefaults
-import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
 @Composable
 fun ActivitasPertama(modifier: Modifier){
-    Column(
-        modifier = Modifier.fillMaxSize(),
+    Column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            stringResource(R.string.prodi),
+        Text(stringResource( R.string.prodi),
             fontSize = 35.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            stringResource(R.string.univ),
-            fontSize = 22.sp
-        )
-        Spacer(modifier = Modifier.height(25.dp))
-        Card (
-            modifier = Modifier
-                .fillMaxWidth(1f)
-                .padding(all = 12.dp),
+            fontWeight = FontWeight.Bold)
+        Text( stringResource( R.string.univ),
+            fontSize = 22.sp)
+        Spacer(modifier = Modifier.height( 25.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth( 1f)
+            .padding( all = 12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )
@@ -56,6 +56,7 @@ fun ActivitasPertama(modifier: Modifier){
                         .padding(5.dp)
                 )
                 Spacer(modifier = Modifier.width(30.dp))
+
                 Column() {
                     Text(
                         stringResource(R.string.nama),
@@ -72,6 +73,17 @@ fun ActivitasPertama(modifier: Modifier){
                     )
                 }
             }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ){
+            Text(
+                stringResource(R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
         }
     }
 }
